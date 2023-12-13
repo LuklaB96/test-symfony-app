949ee3e85162a86d52deb23a9ff02eeeb20ba594
Lukasz <lukasz.bulicz96@gmail.com>
2023-12-13 17:53:13 +0100

    final commit

---
+++ src/Command/CreatePostsDataCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Command;
+
+use App\DataApihHelpers\ApiDataManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: 'app:create-posts-data',
+    description: 'Saving posts from api call'
+)]
+class CreatePostsDataCommand extends Command
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+
+        //if child has constructor, parent constructor will not be called automatically, we calling it explicitly here
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Saves all posts from api.')
+            // the command help shown when running the command with the "--help" option
+            ->setHelp('This command saves all posts from api https://jsonplaceholder.typicode.com/posts to database')
+        ;
+    }
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        //api data manager used to download all the data from https://jsonplaceholder.typicode.com/posts and user names in relation with https://jsonplaceholder.typicode.com/users endpoint
+        $apiDataManager = new ApiDataManager($this->entityManager);
+
+        //download data and get amount of entities created in database
+        $savedPosts = $apiDataManager->SavePostsFromApi();
+
+        //output amount to the console
+        $output->writeln("Saved posts: $savedPosts");
+
+
+        return Command::SUCCESS;
+    }
+}
+
+
+?>
